f82a1c4d600b3069d2b94bb4d812270d93b72846
SolidAlloy <perepelartem@gmail.com>
2021-02-10 21:11:07 +0200

    Started handling custom classes correctly if constant value is chosen

--- Editor/ReferenceDrawer.cs
+++ Editor/ReferenceDrawer.cs
@@ -4,18 +4,18 @@
     using JetBrains.Annotations;
     using SolidUtilities.Editor.Helpers;
     using SolidUtilities.Extensions;
+    using SolidUtilities.UnityEditorInternals;
     using UnityEditor;
     using UnityEngine;
-    using UnityEngine.Assertions;
 
     [CustomPropertyDrawer(typeof(Reference), true)]
     public class ReferenceDrawer : PropertyDrawer
     {
+        private static readonly Dictionary<Object, Editor> _editorCache = new Dictionary<Object, Editor>();
+
         private SerializedProperty _useConstant;
         private SerializedProperty _constantValue;
         private SerializedProperty _variable;
-
-        private static readonly Dictionary<Object, Editor> _editorCache = new Dictionary<Object, Editor>();
 
         private bool UseConstant
         {
@@ -24,6 +24,19 @@
         }
 
         private Object Variable => _variable.objectReferenceValue;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            FindProperties(property);
+
+            if ( ! UseConstant || ! _constantValue.isExpanded)
+                return EditorGUIUtility.singleLineHeight;
+
+            // If a property has a custom property drawer, it will be drown inside a foldout anyway, so we account for
+            // it by adding a single line height.
+            float additionalHeight = _constantValue.HasCustomPropertyDrawer() ? EditorGUIUtility.singleLineHeight : 0f;
+            return EditorGUI.GetPropertyHeight(_constantValue, GUIContent.none) + additionalHeight;
+        }
 
         public override void OnGUI(Rect fieldRect, SerializedProperty property, GUIContent label)
         {
@@ -35,14 +48,14 @@
 
                 DrawLabel(property, fieldRect, labelRect, label);
 
-                // The indent level must be made 0 for button and value to be displayed normally, without any
+                // The indent level must be made 0 for the button and value to be displayed normally, without any
                 // additional indent. Otherwise, the button will not be clickable, and the value will look shifted
                 // compared to other fields.
                 int previousIndent = EditorGUI.indentLevel;
                 EditorGUI.indentLevel = 0;
 
                 UseConstant = ChoiceButton.DrawAndCheckConstant(buttonRect, UseConstant);
-                DrawValue(property, valueRect, previousIndent);
+                DrawValue(property, valueRect, fieldRect, previousIndent);
 
                 EditorGUI.indentLevel = previousIndent;
             }
@@ -51,36 +64,81 @@
         private void FindProperties(SerializedProperty property)
         {
             _useConstant = property.FindPropertyRelative("_useConstant");
-            Assert.IsNotNull(_useConstant);
-
             _constantValue = property.FindPropertyRelative("_constantValue");
-            Assert.IsNotNull(_constantValue);
-
             _variable = property.FindPropertyRelative("_variable");
-            Assert.IsNotNull(_variable);
         }
 
-        private void DrawValue(SerializedProperty property, Rect valueRect, int indentLevel)
+        private void DrawValue(SerializedProperty property, Rect valueRect, Rect totalRect, int indentLevel)
         {
             if (UseConstant)
             {
-                EditorGUI.PropertyField(valueRect, _constantValue, GUIContent.none);
+                DrawConstantValue(valueRect, totalRect, indentLevel);
             }
             else
             {
-                EditorGUI.PropertyField(valueRect, _variable, GUIContent.none);
-
-                if ( ! property.isExpanded || Variable == null)
-                    return;
-
-                using (new EditorDrawHelper.IndentLevel(indentLevel + 2))
-                {
-                    GetInlineEditor(Variable).OnInspectorGUI();
-                }
+                DrawVariableValue(valueRect, property, indentLevel);
             }
         }
 
-        private Editor GetInlineEditor(Object variable)
+        private void DrawConstantValue(Rect valueRect, Rect totalRect, int indentLevel)
+        {
+            if (_constantValue.propertyType == SerializedPropertyType.Generic)
+            {
+                DrawConstantValueInFoldout(totalRect, indentLevel);
+            }
+            else
+            {
+                EditorGUI.PropertyField(valueRect, _constantValue, GUIContent.none);
+            }
+        }
+
+        private void DrawConstantValueInFoldout(Rect totalRect, int indentLevel)
+        {
+            if ( ! _constantValue.isExpanded)
+                return;
+
+            const float paddingBetweenFields = 2f;
+            const float indentPerLevel = 15f;
+
+            totalRect.xMin += (indentLevel + 1) * indentPerLevel;
+            totalRect.y += EditorGUIUtility.singleLineHeight + paddingBetweenFields;
+
+            if (_constantValue.HasCustomPropertyDrawer())
+            {
+                float height = EditorGUI.GetPropertyHeight(_constantValue);
+                totalRect.height = height;
+                EditorGUI.PropertyField(totalRect, _constantValue, GUIContent.none);
+                return;
+            }
+
+            // This draws all child fields of the _constantValue property with indent.
+            SerializedProperty iterator = _constantValue.Copy();
+            var nextProp = _constantValue.Copy();
+            nextProp.NextVisible(false);
+
+            while (iterator.NextVisible(true) && ! SerializedProperty.EqualContents(iterator, nextProp))
+            {
+                float height = EditorGUI.GetPropertyHeight(iterator, false);
+                totalRect.height = height;
+                EditorGUI.PropertyField(totalRect, iterator, true);
+                totalRect.y += height + paddingBetweenFields;
+            }
+        }
+
+        private void DrawVariableValue(Rect valueRect, SerializedProperty property, int indentLevel)
+        {
+            EditorGUI.PropertyField(valueRect, _variable, GUIContent.none);
+
+            if ( ! property.isExpanded || Variable == null)
+                return;
+
+            using (new EditorDrawHelper.IndentLevel(indentLevel + 1))
+            {
+                GetInlineEditor(Variable).OnInspectorGUI();
+            }
+        }
+
+        private static Editor GetInlineEditor(Object variable)
         {
             if ( ! _editorCache.TryGetValue(variable, out Editor editor))
             {
@@ -115,7 +173,14 @@
         {
             if (UseConstant || Variable == null)
             {
-                EditorGUI.HandlePrefixLabel(totalRect, labelRect, label);
+                if (_constantValue.propertyType == SerializedPropertyType.Generic)
+                {
+                    _constantValue.isExpanded = EditorGUI.Foldout(labelRect, _constantValue.isExpanded, label, true);
+                }
+                else
+                {
+                    EditorGUI.HandlePrefixLabel(totalRect, labelRect, label);
+                }
             }
             else
             {
