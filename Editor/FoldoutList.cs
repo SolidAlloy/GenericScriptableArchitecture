4087baccb57e60755b8b3713614c9171e5e552a8
SolidAlloy <perepelartem@gmail.com>
2021-02-24 12:24:40 +0200

    Moved foldout-list-related code to a separate class and started showing the list of listeners only in play mode

---
+++ Editor/FoldoutList.cs
@@ -0,0 +1,73 @@
+﻿namespace GenericScriptableArchitecture.Editor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using GenericUnityObjects.Editor;
+    using UnityEditor;
+    using UnityEditorInternal;
+    using UnityEngine;
+    using Object = UnityEngine.Object;
+
+    internal class FoldoutList<TElement> where TElement : Object
+    {
+        private static readonly MethodInfo _clearCacheMethod = typeof(ReorderableList)
+            .GetMethod("ClearCache", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private readonly ReorderableList _list;
+        private readonly Action _clearCache;
+
+        private Type _realElementType;
+
+        public FoldoutList(List<TElement> elements, SerializedProperty expandedProperty)
+        {
+            _list = new ReorderableList(elements, typeof(TElement), false, true,
+                false, false)
+            {
+                drawHeaderCallback = rect =>
+                {
+                    const float leftMargin = 10f;
+                    var shiftedRight = new Rect(rect.x + leftMargin, rect.y, rect.width - leftMargin, rect.height);
+                    bool newValue = EditorGUI.Foldout(shiftedRight, expandedProperty.boolValue, "Listeners", true);
+
+                    if (expandedProperty.boolValue == newValue)
+                        return;
+
+                    expandedProperty.boolValue = newValue;
+                    _clearCache();
+                },
+                drawElementCallback = (rect, index, isActive, isFocused) =>
+                {
+                    if ( ! expandedProperty.boolValue)
+                    {
+                        GUI.enabled = index == elements.Count;
+                        return;
+                    }
+
+                    var element = elements[index];
+
+                    using (new EditorGUI.DisabledScope(true))
+                        GenericObjectDrawer.ObjectField(rect, null, element, GetRealElementType(element), true);
+                },
+                elementHeightCallback = indexer => expandedProperty.boolValue ? EditorGUIUtility.singleLineHeight : 0f,
+            };
+
+            _clearCache = (Action) Delegate.CreateDelegate(typeof(Action), _list, _clearCacheMethod);
+        }
+
+        public void DoLayoutList()
+        {
+            _list.DoLayoutList();
+        }
+
+        private Type GetRealElementType(TElement element)
+        {
+            if (_realElementType == null)
+            {
+                _realElementType = element.GetType().BaseType;
+            }
+
+            return _realElementType;
+        }
+    }
+}
--- Editor/ScriptableEventEditor.cs
+++ Editor/ScriptableEventEditor.cs
@@ -1,80 +1,20 @@
 ﻿namespace GenericScriptableArchitecture.Editor
 {
-    using System;
-    using System.Reflection;
     using EasyButtons.Editor;
-    using GenericUnityObjects.Editor;
     using GenericUnityObjects.UnityEditorInternals;
     using UnityEditor;
-    using UnityEditorInternal;
-    using UnityEngine;
 
     [CustomEditor(typeof(ScriptableEventBase), true)]
     public class ScriptableEventEditor : GenericHeaderEditor
     {
         private ButtonsDrawer _buttonsDrawer;
-        private ReorderableList _listeners;
-        private ScriptableEventBase _scriptableEventTarget;
-        private Type _listenersType;
-        private SerializedProperty _listenersExpanded;
-
-        private Action _clearCache;
-
-        private static readonly MethodInfo _clearCacheMethod = typeof(ReorderableList).GetMethod("ClearCache",
-            BindingFlags.Instance | BindingFlags.NonPublic);
+        private FoldoutList<ScriptableEventListenerBase> _listenersList;
 
         private void OnEnable()
         {
             _buttonsDrawer = new ButtonsDrawer(target);
-            _scriptableEventTarget = (ScriptableEventBase) target;
-            _listenersExpanded = serializedObject.FindProperty(nameof(ScriptableEventBase.ListenersExpanded));
-
-            _listeners = GetListenersList();
-            _clearCache = (Action) Delegate.CreateDelegate(typeof(Action), _listeners, _clearCacheMethod);
-        }
-
-        private ReorderableList GetListenersList()
-        {
-            return new ReorderableList(_scriptableEventTarget.Listeners, typeof(ScriptableEventListenerBase),
-                false, true, false, false)
-            {
-                drawHeaderCallback = rect =>
-                {
-                    const float leftMargin = 10f;
-                    var shiftedRight = new Rect(rect.x + leftMargin, rect.y, rect.width - leftMargin, rect.height);
-                    bool newValue = EditorGUI.Foldout(shiftedRight, _listenersExpanded.boolValue, "Listeners", true);
-
-                    if (_listenersExpanded.boolValue == newValue)
-                        return;
-
-                    _listenersExpanded.boolValue = newValue;
-                    _clearCache();
-                },
-                drawElementCallback = (rect, index, isActive, isFocused) =>
-                {
-                    if ( ! _listenersExpanded.boolValue)
-                    {
-                        GUI.enabled = index == _scriptableEventTarget.Listeners.Count;
-                        return;
-                    }
-
-                    var listener = _scriptableEventTarget.Listeners[index];
-
-                    using (new EditorGUI.DisabledScope(true))
-                        GenericObjectDrawer.ObjectField(rect, null, listener, GetListenerType(listener), true);
-                },
-                elementHeightCallback = indexer => _listenersExpanded.boolValue ? EditorGUIUtility.singleLineHeight : 0f,
-            };
-        }
-
-        private Type GetListenerType(ScriptableEventListenerBase listener)
-        {
-            if (_listenersType == null)
-            {
-                _listenersType = listener.GetType().BaseType;
-            }
-
-            return _listenersType;
+            var listenersExpanded = serializedObject.FindProperty(nameof(ScriptableEventBase.ListenersExpanded));
+            _listenersList = new FoldoutList<ScriptableEventListenerBase>(((ScriptableEventBase)target).Listeners, listenersExpanded);
         }
 
         public override void OnInspectorGUI()
@@ -89,9 +29,11 @@
 
             _buttonsDrawer.DrawButtons(targets);
 
-            EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
-
-            _listeners.DoLayoutList();
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+                _listenersList.DoLayoutList();
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
