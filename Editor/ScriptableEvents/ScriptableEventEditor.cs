866b656c086b94e43de5c45cc33b0a34a30ebc32
Artem Perepelitsa <perepelartem@gmail.com>
2021-06-26 20:19:28 +0300

    Added StackTraceDrawer

--- Editor/ScriptableEvents/ScriptableEventEditor.cs
+++ Editor/ScriptableEvents/ScriptableEventEditor.cs
@@ -4,26 +4,30 @@
     using GenericUnityObjects.UnityEditorInternals;
     using UnityEditor;
 
-    [CustomEditor(typeof(ScriptableEventBase), true)]
+    [CustomEditor(typeof(BaseScriptableEvent), true)]
     internal class ScriptableEventEditor : GenericHeaderEditor
     {
         private ButtonsDrawer _buttonsDrawer;
-        private FoldoutList<ScriptableEventListenerBase> _listenersList;
+        private FoldoutList<BaseScriptableEventListener> _listenersList;
         private FoldoutList<UnityEngine.Object> _responseTargetsList;
         private SerializedProperty _description;
+        private StackTraceDrawer _stackTrace;
 
         private void OnEnable()
         {
             _buttonsDrawer = new ButtonsDrawer(target);
-            var typedTarget = (ScriptableEventBase) target;
+            var typedTarget = (BaseScriptableEvent) target;
 
-            var listenersExpanded = serializedObject.FindProperty(nameof(ScriptableEventBase.ListenersExpanded));
-            _listenersList = new FoldoutList<ScriptableEventListenerBase>(typedTarget.Listeners, listenersExpanded, "Listeners");
+            var listenersExpanded = serializedObject.FindProperty(nameof(BaseScriptableEvent.ListenersExpanded));
+            _listenersList = new FoldoutList<BaseScriptableEventListener>(typedTarget.Listeners, listenersExpanded, "Listeners");
 
-            var responseTargetsExpanded = serializedObject.FindProperty(nameof(ScriptableEventBase.ResponseTargetsExpanded));
+            var responseTargetsExpanded = serializedObject.FindProperty(nameof(BaseScriptableEvent.ResponseTargetsExpanded));
             _responseTargetsList = new FoldoutList<UnityEngine.Object>(typedTarget.ResponseTargets, responseTargetsExpanded, "Response Targets");
 
             _description = serializedObject.FindProperty("_description");
+
+            _stackTrace = new StackTraceDrawer(typedTarget);
+            _stackTrace.OnRepaint.AddListener(Repaint);
         }
 
         public override void OnInspectorGUI()
@@ -43,6 +47,8 @@
             EditorGUILayout.Space(EditorGUIUtility.singleLineHeight / 2);
             _listenersList.DoLayoutList();
             _responseTargetsList.DoLayoutList();
+
+            _stackTrace.Draw();
         }
     }
 }
