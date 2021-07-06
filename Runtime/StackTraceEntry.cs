01844dcf2222893e637b441fdb4b749be36d6791
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-06 22:55:53 +0300

    Added links to the stack trace

--- Runtime/StackTraceEntry.cs
+++ Runtime/StackTraceEntry.cs
@@ -2,6 +2,9 @@
 {
     using System;
     using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using SolidUtilities.Extensions;
     using UnityEngine;
 
     public class StackTraceEntry : IEquatable<StackTraceEntry>
@@ -58,7 +61,42 @@
         public override string ToString()
         {
             var valuesString = string.Join(", ", _values.Select(value => value ?? "null"));
-            return $"{_frameCount}{(_values.Length != 0 ? $"   [{valuesString}]" : string.Empty)} {_stackTrace}";
+            return $"{_frameCount}{(_values.Length != 0 ? $"   [{valuesString}]" : string.Empty)} {GetStackTraceWithLinks(_stackTrace)}";
+        }
+
+        private string GetStackTraceWithLinks(string stackTrace)
+        {
+            string unityStackTrace = TransformStackTraceToUnity(stackTrace);
+            return AddLinks(unityStackTrace);
+        }
+
+        private string TransformStackTraceToUnity(string stackTrace)
+        {
+            // Remove first three lines
+            int thirdLineEnd = stackTrace.IndexOfNth('\n', 2);
+            stackTrace = stackTrace.Remove(0, thirdLineEnd + 1);
+
+            // Remove at at the start of lines
+            stackTrace = Regex.Replace(stackTrace, @" +at ", string.Empty);
+
+            // Remove the location part of the stack line if there is no reference to a file on disc
+            stackTrace = Regex.Replace(stackTrace, @" \[0x00000\] in <.*?(?=\n|$)", string.Empty);
+
+            // Replace [0x00000] in with (at
+            stackTrace = Regex.Replace(stackTrace, @"\[0x\w+?\] in", "(at");
+
+            // Add a closing parenthese
+            stackTrace = Regex.Replace(stackTrace, @"(?<=:\d+) ", ")");
+
+            return stackTrace;
+        }
+
+        private string AddLinks(string unityStackTrace)
+        {
+            var assembly = typeof(UnityEditor.Editor).Assembly;
+            var consoleWindow = assembly.GetType("UnityEditor.ConsoleWindow");
+            var method = consoleWindow.GetMethod("StacktraceWithHyperlinks", BindingFlags.NonPublic | BindingFlags.Static);
+            return (string) method.Invoke(null, new object[] {unityStackTrace, 0});
         }
 
         public static implicit operator string(StackTraceEntry trace) => trace.ToString();
