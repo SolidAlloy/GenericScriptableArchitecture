3e0bdef9b8dbb22c95f492fe96e7d158975010d1
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-17 15:09:41 +0300

    Started refactoring StackTraceDrawer

--- Editor/StackTraceDrawer.cs
+++ Editor/StackTraceDrawer.cs
@@ -1,23 +1,17 @@
 ﻿namespace GenericScriptableArchitecture.Editor
 {
-    using System;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using JetBrains.Annotations;
+    using SolidUtilities.Editor.Helpers;
+    using SolidUtilities.Helpers;
     using UnityEditor;
-    using UnityEditor.Experimental.GraphView;
     using UnityEditorInternal;
     using UnityEngine;
 
     internal class StackTraceDrawer
     {
-        private const float Height = 400;
-        private const float HeaderHeight = 18;
-        private const float ResizeMargin = 0.2f;
-        private const float ResizeHeight = 4;
-        private const float ClearLeftPadding = 6;
-        private const float ClearWidth = 45;
-        private const float CollapseWidth = 55;
-        private const float LineHeight = 18;
+        private static readonly ContentCache _contentCache = new ContentCache();
 
         private readonly IStackTraceProvider _target;
         private readonly IRepaintable _editor;
@@ -25,12 +19,12 @@
         private StackTraceEntry _selectedTrace;
 
         private Rect _lastContentRect;
-        private Vector2 _listScrollPosition;
-        private Vector2 _contentScrollPosition;
-        private float _subWindowValue = 0.6f;
+        private Vector2 _listScrollPos;
+        private Vector2 _selectedContentScrollPos;
+        private float _windowsHeightRatio = 0.6f;
         private bool _resizeMouseDown;
 
-        public StackTraceDrawer([NotNull] IStackTraceProvider target, [NotNull] IRepaintable editor, bool startCollapsed = false)
+        public StackTraceDrawer([NotNull] IStackTraceProvider target, [NotNull] IRepaintable editor)
         {
             _target = target;
             _editor = editor;
@@ -38,19 +32,16 @@
 
         public void Draw()
         {
+            const float contentRectHeight = 400;
+            const float headerHeight = 18;
+
             EditorGUILayout.Space();
-
-            GUILayout.BeginVertical(Array.Empty<GUILayoutOption>());
-
-            Rect headerRect = GUILayoutUtility.GetRect(0f, HeaderHeight, GUILayout.ExpandWidth(true));
-            Rect contentRect = GUILayoutUtility.GetRect(10f, _target.Expanded ? Height : 0f, GUILayout.ExpandWidth(true));
+            using var verticalBlock = new DrawHelper.VerticalBlock(null);
+            Rect headerRect = GUILayoutUtility.GetRect(0f, headerHeight, GUILayout.ExpandWidth(true));
+            Rect contentRect = GUILayoutUtility.GetRect(10f, _target.Expanded ? contentRectHeight : 0f, GUILayout.ExpandWidth(true));
             DrawHeader(headerRect);
             DrawContent(contentRect);
-
-            GUILayout.EndVertical();
         }
-
-        #region ReorederableList
 
         private void DrawHeader(Rect headerRect)
         {
@@ -67,16 +58,26 @@
             DrawFoldout(headerRect);
         }
 
-        private const string HeaderTitle = "Stack Trace";
-
         private void DrawFoldout(Rect headerRect)
         {
+            const float buttonWidth = 60f;
             const float leftMargin = 10f;
-            var shiftedRight = new Rect(headerRect.x + leftMargin, headerRect.y, headerRect.width - leftMargin, headerRect.height);
-            _target.Expanded = EditorGUI.Foldout(shiftedRight, _target.Expanded, HeaderTitle, true);
-        }
+            var shiftedRightHeaderRect = new Rect(headerRect.x + leftMargin, headerRect.y, headerRect.width - leftMargin, headerRect.height);
 
-        #endregion
+            var buttonRect = new Rect(
+                shiftedRightHeaderRect.xMax - buttonWidth,
+                shiftedRightHeaderRect.y + 1f,
+                buttonWidth,
+                shiftedRightHeaderRect.height);
+
+            if (GUI.Button(buttonRect, "Clear"))
+            {
+                _target.Entries.Clear();
+                _selectedTrace = null;
+            }
+
+            _target.Expanded = EditorGUI.Foldout(shiftedRightHeaderRect, _target.Expanded, "Stack Trace", true);
+        }
 
         private void DrawContent(Rect contentRect)
         {
@@ -91,9 +92,11 @@
 
             GUILayout.BeginArea(_lastContentRect);
 
-            _subWindowValue = Mathf.Clamp(_subWindowValue, ResizeMargin, 1 - ResizeMargin);
+            const float resizeMargin = 0.2f;
 
-            var splitHeight = _lastContentRect.height * _subWindowValue;
+            _windowsHeightRatio = Mathf.Clamp(_windowsHeightRatio, resizeMargin, 1 - resizeMargin);
+
+            var splitHeight = _lastContentRect.height * _windowsHeightRatio;
             contentRect = new Rect(-1, splitHeight, _lastContentRect.width + 1, _lastContentRect.height - splitHeight);
 
             DrawList(contentRect);
@@ -105,6 +108,8 @@
 
         private void DrawList(Rect contentRect)
         {
+            const float lineHeight = 18f;
+
             var listRect = new Rect
             {
                 height = _lastContentRect.height - contentRect.height,
@@ -120,11 +125,11 @@
 
             var position = new Rect
             {
-                height = _target.Entries.Count * LineHeight,
+                height = _target.Entries.Count * lineHeight,
                 width = scrollViewRect.width - 20,
             };
 
-            _listScrollPosition = GUI.BeginScrollView(scrollViewRect, _listScrollPosition, position);
+            _listScrollPos = GUI.BeginScrollView(scrollViewRect, _listScrollPos, position);
 
             int i = 0;
 
@@ -135,8 +140,8 @@
                 var elementRect = new Rect
                 {
                     width = listRect.width,
-                    height = LineHeight,
-                    y = i * LineHeight,
+                    height = lineHeight,
+                    y = i * lineHeight,
                     x = listRect.x
                 };
 
@@ -179,27 +184,39 @@
                 width = contentRect.width - 1,
             };
 
-            Vector2 textSize = Styles.MessageStyle.CalcSize(new GUIContent(_selectedTrace));
+            var textWidth = scrollViewRect.width - 14;
 
-            var position = new Rect(Vector2.zero, textSize);
+            var lines = _selectedTrace.ToString().Split('\n');
+            var lineHeights = lines.Select(line => Styles.MessageStyle.CalcHeight(_contentCache.GetItem(line), textWidth)).ToList();
 
-            _contentScrollPosition = GUI.BeginScrollView(scrollViewRect, _contentScrollPosition, position);
+            var listRect = new Rect(0f, 0f, textWidth, lineHeights.Sum());
+            _selectedContentScrollPos = GUI.BeginScrollView(scrollViewRect, _selectedContentScrollPos, listRect);
 
-            // TODO: Try using ConsoleWindow.StackTraceWithHyperLinks(string stackTrace)
-            // If successful, embed it into StackTraceEntry
-            EditorGUI.SelectableLabel(position, _selectedTrace, Styles.MessageStyle);
+            var currentYPos = 0f;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var lineHeight = lineHeights[i];
+                var lineRect = new Rect(0f, currentYPos, textWidth, lineHeight);
+                currentYPos += lineHeight;
+
+                EditorGUI.SelectableLabel(lineRect, lines[i], Styles.MessageStyle);
+                DrawSeparator(lineRect);
+            }
 
             GUI.EndScrollView();
         }
 
         private void HandleSelectedPartResize(float splitHeight)
         {
-            var cursorRect = new Rect(0, splitHeight - ResizeHeight / 2, _lastContentRect.width, ResizeHeight);
+            const float resizeHeight = 4f;
+
+            var cursorRect = new Rect(0, splitHeight - resizeHeight / 2, _lastContentRect.width, resizeHeight);
             Event currentEvent = Event.current;
 
             EditorGUIUtility.AddCursorRect(cursorRect, MouseCursor.ResizeVertical);
 
-            var lineRect = new Rect(0, splitHeight - ResizeHeight / 4, _lastContentRect.width, ResizeHeight / 2);
+            var lineRect = new Rect(0, splitHeight - resizeHeight / 4, _lastContentRect.width, resizeHeight / 2);
             EditorGUI.DrawRect(lineRect, Styles.DarkSeparatorLine);
 
             if (currentEvent.type == EventType.MouseDown && cursorRect.Contains(currentEvent.mousePosition))
@@ -212,66 +229,34 @@
             }
             else if (_resizeMouseDown)
             {
-                _subWindowValue = currentEvent.mousePosition.y / _lastContentRect.height;
+                _windowsHeightRatio = currentEvent.mousePosition.y / _lastContentRect.height;
                 Repaint();
             }
         }
 
-        // private void DrawStackTraceHeader()
-        // {
-        //     var rect = new Rect
-        //     {
-        //         width = _stackTraceRect.width,
-        //         height = HeaderHeight,
-        //     };
-        //
-        //     if (Event.current.type == EventType.Repaint)
-        //     {
-        //         Styles.Header.Draw(rect, new GUIContent("Stack Trace"), 0);
-        //     }
-        //
-        //     rect.x += ClearLeftPadding;
-        //     rect.width = ClearWidth;
-        //
-        //     if (GUI.Button(rect, new GUIContent("Clear"), Styles.HeaderButton))
-        //     {
-        //         _target.Entries.Clear();
-        //         Deselect();
-        //     }
-        //
-        //     rect.x += ClearWidth;
-        //     rect.width = CollapseWidth;
-        //
-        //     _collapseAnimation.target = !GUI.Toggle(rect, !_collapseAnimation.target, new GUIContent("Collapse"), Styles.HeaderButton);
-        // }
+        private static string GetFirstLine(string value)
+        {
+            return Regex.Match(value, @".*?(\r|\n)").Value;
+        }
 
         private void Repaint() => _editor.Repaint();
 
-        private static string GetFirstLine(string value)
-        {
-            return value.Split('\r', '\n').FirstOrDefault();
-        }
-        private void Deselect()
-        {
-            _selectedTrace = null;
-        }
-
         private static class Styles
         {
-            public static readonly GUIStyle Text = new GUIStyle("CN EntryInfo") { padding = new RectOffset(2, 2, 3, 0)};
+            public static readonly GUIStyle Text = new GUIStyle("CN EntryInfo") { padding = new RectOffset(2, 2, 3, 0) };
             public static readonly GUIStyle Background = new GUIStyle("CN EntryBackodd");
             public static readonly GUIStyle MessageStyle = "CN Message";
             public static readonly GUIStyle BoxBackground = "RL Background";
 
-            private static readonly Color DarkSeparatorLineDarkSkin = new Color(0.11f, 0.11f, 0.11f, 0.258f);
-            private static readonly Color DarkSeparatorLineLightSkin = new Color(0.0f, 0.0f, 0.0f, 0.065f);
+            private static readonly Color _darkSeparatorLineDarkSkin = new Color(0.11f, 0.11f, 0.11f, 0.258f);
+            private static readonly Color _darkSeparatorLineLightSkin = new Color(0.0f, 0.0f, 0.0f, 0.065f);
 
-            private static readonly Color LightSeparatorLineDarkSkin = new Color(1f, 1f, 1f, 0.033f);
-            private static readonly Color LightSeparatorLineLightSkin = new Color(1f, 1f, 1f, 0.323f);
+            private static readonly Color _lightSeparatorLineDarkSkin = new Color(1f, 1f, 1f, 0.033f);
+            private static readonly Color _lightSeparatorLineLightSkin = new Color(1f, 1f, 1f, 0.323f);
 
-            public static Color DarkSeparatorLine => DarkSkin ? DarkSeparatorLineDarkSkin : DarkSeparatorLineLightSkin;
+            public static Color DarkSeparatorLine => DarkSkin ? _darkSeparatorLineDarkSkin : _darkSeparatorLineLightSkin;
 
-            public static Color LightSeparatorLine => DarkSkin ? LightSeparatorLineDarkSkin : LightSeparatorLineLightSkin;
+            public static Color LightSeparatorLine => DarkSkin ? _lightSeparatorLineDarkSkin : _lightSeparatorLineLightSkin;
 
             private static bool DarkSkin => EditorGUIUtility.isProSkin;
         }
