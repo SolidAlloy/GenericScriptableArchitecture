c3bea1228d405798a488b0181d758c4439fc469c
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-24 17:02:56 +0300

    Added project settings that allows enabling/disabling of stack traces

---
+++ Editor/StackTrace/SettingsDrawer.cs
@@ -0,0 +1,56 @@
+﻿namespace GenericScriptableArchitecture.Editor
+{
+    using System.Collections.Generic;
+    using SolidUtilities.Editor.Helpers;
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class SettingsDrawer
+    {
+        private const string EnabledInProjectLabel = "Enable stack traces in all assets";
+
+        private static readonly GUIContent _enabledInProjectContent = new GUIContent(EnabledInProjectLabel,
+            "This has higher priority than the disabled stack trace option on individual assets.");
+
+        [SettingsProvider]
+        public static SettingsProvider CreateSettingsProvider()
+        {
+            return new SettingsProvider("Project Settings/Generic ScriptableObject Architecture", SettingsScope.Project)
+            {
+                guiHandler = OnGUI,
+                keywords = GetKeywords()
+            };
+        }
+
+        private static void OnGUI(string searchContext)
+        {
+            if (StackTraceSettings.EnabledInProject)
+            {
+                EditorGUILayoutHelper.DrawInfoMessage(
+                    "Stack traces may affect performance of the game in editor. If possible, enable it on individual assets when they require debugging.");
+            }
+
+            using (EditorGUIUtilityHelper.LabelWidthBlock(220f))
+            {
+                StackTraceSettings.EnabledInProject = EditorGUILayout.Toggle(_enabledInProjectContent, StackTraceSettings.EnabledInProject);
+            }
+        }
+
+        private static HashSet<string> GetKeywords()
+        {
+            var keywords = new HashSet<string>();
+            keywords.AddWords(EnabledInProjectLabel);
+            return keywords;
+        }
+
+        private static readonly char[] _separators = { ' ' };
+
+        private static void AddWords(this HashSet<string> set, string phrase)
+        {
+            foreach (string word in phrase.Split(_separators))
+            {
+                set.Add(word);
+            }
+        }
+    }
+}
---
+++ Editor/StackTrace/StackTraceSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericScriptableArchitecture.Editor
+{
+    using UnityEditor;
+    using UnityEditor.SettingsManagement;
+
+    internal static class StackTraceSettings
+    {
+        private const string PackageName = "com.solidalloy.generic-scriptable-architecture";
+
+        private static Settings _instance;
+
+        private static UserSetting<bool> _enabledInProject;
+
+        public static bool EnabledInProject
+        {
+            get
+            {
+                InitializeIfNeeded();
+                return _enabledInProject.value;
+            }
+
+            set => _enabledInProject.value = value;
+        }
+
+        private static void InitializeIfNeeded()
+        {
+            if (_instance != null)
+                return;
+
+            _instance = new Settings(PackageName);
+
+            _enabledInProject = new UserSetting<bool>(_instance, nameof(_enabledInProject), false, SettingsScope.User);
+        }
+    }
+}
