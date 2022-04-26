b34d0bf4f936d91e9914c0013eb88cd83f757c05
Artem <perepelartem@gmail.com>
2022-04-26 22:06:43 +0300

    refactor: Removed DrawerWithModes

--- Editor/ReferenceDrawer.cs
+++ Editor/ReferenceDrawer.cs
@@ -2,16 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
+    using SolidUtilities;
     using SolidUtilities.Editor;
-    using SolidUtilities.Editor.PropertyDrawers;
+    using SolidUtilities.UnityEditorInternals;
     using UnityEditor;
     using UnityEngine;
     using Object = UnityEngine.Object;
 
     [CustomPropertyDrawer(typeof(ReferenceBase), true)]
-    internal class ReferenceDrawer : DrawerWithModes
+    internal class ReferenceDrawer : PropertyDrawer
     {
         private static readonly Dictionary<Object, Editor> _editorCache = new Dictionary<Object, Editor>();
+        
+        private static GUIStyle _buttonStyle;
+        private static GUIStyle ButtonStyle => _buttonStyle ??= new GUIStyle(GUI.skin.GetStyle("PaneOptions"))
+        {
+            imagePosition = ImagePosition.ImageOnly
+        };
 
         private SerializedProperty _mainProperty;
         private SerializedProperty _valueType;
@@ -19,13 +26,13 @@
         private SerializedProperty _variable;
         private SerializedProperty _constant;
 
-        protected override int PopupValue
+        private int PopupValue
         {
             get => _valueType.enumValueIndex;
             set => _valueType.enumValueIndex = value;
         }
 
-        protected override SerializedProperty ExposedProperty
+        private SerializedProperty ExposedProperty
         {
             get
             {
@@ -39,11 +46,7 @@
             }
         }
 
-        protected override bool ShouldDrawFoldout =>
-            ValueType != ReferenceBase.ValueTypes.Value && ObjectReference != null || _value.propertyType == SerializedPropertyType.Generic;
-
         private static readonly string[] _popupOptions = { "Value", "Constant", "Variable" };
-        protected override string[] PopupOptions => _popupOptions;
 
         private ReferenceBase.ValueTypes ValueType => (ReferenceBase.ValueTypes) PopupValue;
 
@@ -53,19 +56,32 @@
         {
             FindProperties(property);
 
-            if ( ValueType != ReferenceBase.ValueTypes.Value)
+            if (ValueType != ReferenceBase.ValueTypes.Value || ! property.isExpanded)
                 return EditorGUIUtility.singleLineHeight;
 
-            return base.GetPropertyHeight(property, label);
+            // If a property has a custom property drawer, it will be drown inside a foldout anyway, so we account for
+            // it by adding a single line height.
+            float additionalHeight = ExposedProperty.HasCustomPropertyDrawer() ? EditorGUIUtility.singleLineHeight : 0f;
+            return EditorGUI.GetPropertyHeight(ExposedProperty, GUIContent.none) + additionalHeight;
         }
 
         public override void OnGUI(Rect fieldRect, SerializedProperty property, GUIContent label)
         {
             FindProperties(property);
-            base.OnGUI(fieldRect, property, label);
+            (Rect labelRect, Rect buttonRect, Rect valueRect) = GetLabelButtonValueRects(fieldRect);
+            DrawLabel(property, fieldRect, labelRect, label);
+
+            // The indent level must be made 0 for the button and value to be displayed normally, without any
+            // additional indent. Otherwise, the button will not be clickable, and the value will look shifted
+            // compared to other fields.
+            int previousIndent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            PopupValue = DrawButton(buttonRect, PopupValue);
+            DrawValue(property, valueRect, fieldRect, previousIndent);
+            EditorGUI.indentLevel = previousIndent;
         }
 
-        protected override void DrawValue(SerializedProperty property, Rect valueRect, Rect totalRect, int indentLevel)
+        private void DrawValue(SerializedProperty property, Rect valueRect, Rect totalRect, int indentLevel)
         {
             switch (ValueType)
             {
@@ -114,6 +130,82 @@
             {
                 GetInlineEditor(ObjectReference).OnInspectorGUI();
             }
+        }
+        
+        private void DrawValueProperty(SerializedProperty mainProperty, Rect valueRect, Rect totalRect, int indentLevel)
+        {
+            if (ExposedProperty.propertyType == SerializedPropertyType.Generic)
+            {
+                DrawValueInFoldout(mainProperty, ExposedProperty, totalRect, indentLevel);
+            }
+            else
+            {
+                EditorGUI.PropertyField(valueRect, ExposedProperty, GUIContent.none);
+            }
+        }
+
+        private void DrawLabel(SerializedProperty property, Rect totalRect, Rect labelRect, GUIContent label)
+        {
+            if (ValueType != ReferenceBase.ValueTypes.Value && ObjectReference != null || _value.propertyType == SerializedPropertyType.Generic)
+            {
+                property.isExpanded = EditorGUI.Foldout(labelRect, property.isExpanded, label, true);
+            }
+            else
+            {
+                EditorGUI.HandlePrefixLabel(totalRect, labelRect, label);
+            }
+        }
+
+        private (Rect label, Rect button, Rect value) GetLabelButtonValueRects(Rect totalRect)
+        {
+            const float indentWidth = 15f;
+            const float valueLeftIndent = 2f;
+
+            totalRect.height = EditorGUIUtility.singleLineHeight;
+
+            (Rect labelAndButtonRect, Rect valueRect) = totalRect.CutVertically(EditorGUIUtility.labelWidth);
+
+            labelAndButtonRect.xMin += EditorGUI.indentLevel * indentWidth;
+
+            (Rect labelRect, Rect buttonRect) =
+                labelAndButtonRect.CutVertically(ButtonStyle.fixedWidth, fromRightSide: true);
+
+            valueRect.xMin += valueLeftIndent;
+            return (labelRect, buttonRect, valueRect);
+        }
+
+        private static void DrawValueInFoldout(SerializedProperty mainProperty, SerializedProperty valueProperty, Rect totalRect, int indentLevel)
+        {
+            valueProperty.isExpanded = mainProperty.isExpanded;
+
+            if ( ! mainProperty.isExpanded)
+                return;
+
+            var shiftedRect = totalRect.ShiftOneLineDown(indentLevel + 1);
+
+            if (valueProperty.HasCustomPropertyDrawer())
+            {
+                shiftedRect.height = EditorGUI.GetPropertyHeight(valueProperty);
+                EditorGUI.PropertyField(shiftedRect, valueProperty, GUIContent.none);
+                return;
+            }
+
+            // This draws all child fields of the _constantValue property with indent.
+            SerializedProperty iterator = valueProperty.Copy();
+            var nextProp = valueProperty.Copy();
+            nextProp.NextVisible(false);
+
+            while (iterator.NextVisible(true) && ! SerializedProperty.EqualContents(iterator, nextProp))
+            {
+                shiftedRect.height = EditorGUI.GetPropertyHeight(iterator, false);
+                EditorGUI.PropertyField(shiftedRect, iterator, true);
+                shiftedRect = shiftedRect.ShiftOneLineDown(lineHeight: shiftedRect.height);
+            }
+        }
+
+        private int DrawButton(Rect buttonRect, int currentValue)
+        {
+            return EditorGUI.Popup(buttonRect, currentValue, _popupOptions, ButtonStyle);
         }
     }
 }
