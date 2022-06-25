c3b5b348a82258ee8b6d7f03ffb337f9e4340b76
Artem <perepelartem@gmail.com>
2022-06-25 21:53:43 +0300

    fix: Added the missing enabled checkbox to ScriptableEventListener component in inspector

--- Runtime/ScriptableEventListeners/ScriptableEventListener.cs
+++ Runtime/ScriptableEventListeners/ScriptableEventListener.cs
@@ -2,8 +2,21 @@
 {
     using UnityEngine;
 
+    [ExecuteAlways] // Needed so that if the component is disabled in edit mode, it also disables the hidden component.
     public class ScriptableEventListener : MonoBehaviour
     {
         [SerializeField] internal BaseScriptableEventListener _component;
+
+        private void OnEnable()
+        {
+            if (_component != null)
+                _component.enabled = true;
+        }
+
+        private void OnDisable()
+        {
+            if (_component != null)
+                _component.enabled = false;
+        }
     }
 }
