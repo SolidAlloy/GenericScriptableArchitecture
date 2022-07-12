621c703e48fe08299f1d3d28b245fee1f3feb0a9
SolidAlloy <perepelartem@gmail.com>
2022-07-12 19:48:45 +0300

    Fixed null-ref exception when subscribing to event instancers from scriptable event listener

--- Runtime/ScriptableEvents/BaseEventInstancer.cs
+++ Runtime/ScriptableEvents/BaseEventInstancer.cs
@@ -2,6 +2,9 @@
 {
     using UnityEngine;
 
+    // Such an execution order allows the instance to be awaken before all the default classes including those predefined in execution order by Unity (like EventSystem with -1000 order).
+    // It's ok that instancers are awaken even before EventSystem because they don't depend on any of those classes.
+    [DefaultExecutionOrder(-2000)]
     public abstract class BaseEventInstancer : MonoBehaviour
     {
         internal abstract ScriptableEventHelper ScriptableEventHelper { get; }
